664a1e1cbbaa9a74b4a284717b4d349c39d8fc3a
EliasTristi <elias.triest@gmail.com>
2024-03-06 12:05:13 +0100

    Initial commit

---
+++ Assets/Scripts/Testing/Card.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private Animator _animator;
+
+    void Start()
+    {
+        //_animator = FindObjectOfType<Animator>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Hello");
+        _animator.Play("Shown");
+    }
+}
